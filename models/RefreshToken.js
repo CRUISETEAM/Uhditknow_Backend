5db68c26f187db79c232aee05b40a36a8a12b4f3
cys070124 <cys070124@gmail.com>
2024-10-17 21:42:01 +0900

    Feat : RefreshToken model 추가

---
+++ models/RefreshToken.js
@@ -0,0 +1,40 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+//refreshtoken
+const refreshToken = sequelize.define('RefreshToken', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    usercode: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'User',
+          key: 'usercode'
+        }
+    },
+    refreshtoken: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    expires_at: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    created_at: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    },
+      revoked: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    }
+    }, {
+      tableName: 'refresh_token',
+      timestamps: false
+});
+
+module.exports = RefreshToken;
