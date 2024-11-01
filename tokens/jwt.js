d3de09732420f6b73fd000dcf9cc25ea310d6077
cys070124 <cys070124@gmail.com>
2024-11-02 00:06:03 +0900

    Feat : Refresh Token 발급 함수 추가

--- tokens/jwt.js
+++ tokens/jwt.js
@@ -1,9 +1,11 @@
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
+const RefreshToken = require('../models/RefreshToken')
 
 // 설정
 const JWT_SECRET = process.env.JWT_SECRET
 const ACCESS_TOKEN_EXPIRATION = process.env.ACCESS_TOKEN_EXPIRATION
+const REFRESH_TOKEN_EXPIRATION = process.env.REFRESH_TOKEN_EXPIRATION
 
 // 액세스 토큰 발급
 const generateAccessToken = (user) => {
@@ -14,4 +16,24 @@ const generateAccessToken = (user) => {
     )
 }
 
-module.exports = { generateAccessToken }
+// 리프레시 토큰 발급
+const generateRefreshToken = async (user) => {
+    const refreshToken = jwt.sign(
+        { usercode: user.usercode },
+        JWT_SECRET,
+        { expiresIn: REFRESH_TOKEN_EXPIRATION }
+    )
+    
+    // 리프레시 토큰 저장
+    await RefreshToken.create({
+        usercode: user.usercode,
+        refreshtoken: refreshToken,
+        expires_at: new Date(Date.now() + 1209600000), // 만료 시간
+        created_at: new Date(),
+        revoked: false
+    })
+ 
+    return refreshToken
+}
+
+module.exports = { generateAccessToken, generateRefreshToken }
