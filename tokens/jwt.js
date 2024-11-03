3055bd8e5902787b9886fac6f812272f72b729fd
cys070124 <cys070124@gmail.com>
2024-11-03 13:06:18 +0900

    Feat : Access Token 발급 함수 수정, role 추가

--- tokens/jwt.js
+++ tokens/jwt.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
+const User = require('../models/User')
 const RefreshToken = require('../models/RefreshToken')
 
 // 설정
@@ -8,9 +9,10 @@ const ACCESS_TOKEN_EXPIRATION = process.env.ACCESS_TOKEN_EXPIRATION
 const REFRESH_TOKEN_EXPIRATION = process.env.REFRESH_TOKEN_EXPIRATION
 
 // 액세스 토큰 발급
-const generateAccessToken = (user) => {
+const generateAccessToken = async (user) => {
+    const userData = await User.findByUserCode(user.usercode)
     return jwt.sign(
-        { usercode: user.usercode, email: user.email }, // 포함할 정보
+        { usercode: user.usercode, email: userData.email, role: userData.role }, // 포함할 정보
         JWT_SECRET, // 시크릿
         { expiresIn: ACCESS_TOKEN_EXPIRATION } // 만료 시간
     )
