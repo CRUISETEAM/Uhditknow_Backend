b80b76ef9a3e2c05622172f2e92b703244b6a00f
cys070124 <cys070124@gmail.com>
2024-11-06 14:20:44 +0900

    Refactor : Access Token 재발급 시 JSON 형식 응답으로 수정

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -5,7 +5,7 @@ const RefreshToken = require('../models/RefreshToken')
 
 const JWT_SECRET = process.env.JWT_SECRET
 
-// 미들웨어: 엑세스 토큰 검증
+// 엑세스 토큰 검증
 const authenticateToken = async (req, res, next) => {
     const authHeader = req.headers['authorization'] // 헤더 Authorization 추출
     const accessToken = authHeader && authHeader.split(' ')[1] // Bearer 토큰 추출
@@ -19,20 +19,21 @@ const authenticateToken = async (req, res, next) => {
 
             try {
                 const decoded = jwt.verify(refreshToken, JWT_SECRET)
-
                 const refreshTokenDB = await RefreshToken.findOne({where: {usercode: decoded.usercode, refreshtoken: refreshToken}})
+
                 if(!refreshTokenDB) return res.sendStatus(403) // 토큰이 없음
 
                 const newAccessToken = await JWT.generateAccessToken(decoded)
                 req.user = { usercode: decoded.usercode }
                 console.log(newAccessToken)
-                res.setHeader('authorization', newAccessToken);
+                return res.json({ accessToken: newAccessToken })
             } catch (refreshError) {
                 return res.sendStatus(403) // 토큰이 유효하지 않으면 403 Forbidden 응답
             }
+        } else {
+            req.user = user
+            next()
         }
-        req.user = user // 검증된 사용자 정보를 요청 객체에 추가
-        next() // 다음 미들웨어 또는 라우트 핸들러로 진행
     })
 }
 
