fa64b48467bcbb86e6399c379adb4a763e856d1d
cys070124 <cys070124@gmail.com>
2024-11-23 11:40:05 +0900

    Feat : 이미지 경로 추가

--- models/Got.js
+++ models/Got.js
@@ -28,6 +28,10 @@ const Got = sequelize.define('Got', {
     type: DataTypes.STRING,
     allowNull: true
   },
+  img: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
 }, {
   tableName: 'got',
   timestamps: false // createdAt, updatedAt 사용 여부
